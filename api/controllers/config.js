1a0c9df611075c1252eb634386444b7e8561e07a
Karen Maravi <karen.maravi@acceso.com.pe>
2022-05-23 15:17:40 -0500

    new ID CLIENT = 585956977825

--- api/controllers/config.js
+++ api/controllers/config.js
@@ -1,7 +1,7 @@
 var axios = require('axios');
 const clienteMongo = require('../database/mongo')
 
-const token = async (req, res = response) => {
+const refresh_token = async (req, res = response) => {
     try {
         console.log('TOKEN')
         const mdb = await clienteMongo.GET_ONE_LATEST_TIME('token_multivende', {}) 
@@ -16,8 +16,8 @@ const token = async (req, res = response) => {
         console.log('refreshToken:',mdb[0].refreshToken)
 
         var data = {
-            client_id: 166355247921,
-            client_secret: "dKPFHHmLhgNNsE6bGKRzTadSovsxblyPsEWd4q6mc1xn57Aw5a",
+            client_id: 585956977825,
+            client_secret: "LXDKAfFzh7cIUUkBL4LtAYSZM8nweAgnDLJekK2zZdr2BavODC",
             grant_type: "refresh_token",
             refresh_token: mdb[0].refreshToken
         // "code": "ac-c2d0b749-d3e8-4ee7-a4cf-ee02fa380333"
@@ -57,6 +57,62 @@ const token = async (req, res = response) => {
     }
 }
 
+const token = async (req, res = response) => {
+    try {
+        console.log('TOKEN')
+        const mdb = await clienteMongo.GET_ONE_LATEST_TIME('token_multivende', {}) 
+
+        if(JSON.stringify(mdb) == '[]') {
+            console.log('NO EXISTE DATA')
+            throw {
+                ok: false,
+                msg: 'Error de envio de data'
+            }
+        }
+        console.log('refreshToken:',mdb[0].refreshToken)
+
+        var data = {
+            client_id: 585956977825,
+            client_secret: "LXDKAfFzh7cIUUkBL4LtAYSZM8nweAgnDLJekK2zZdr2BavODC",
+            grant_type: "authorization_code",
+            code: "ac-4f3970de-3711-4086-a988-b9129b7645f2"
+        }
+
+        var config = {
+            method: 'post',
+            //baseURL: 'https://app.multivende.com',
+            url: 'https://app.multivende.com/oauth/access-token',
+            headers: { 
+                'cache-control': 'no-cache', 
+                'Content-Type': 'application/json'
+            },
+            data : data
+        };
+
+        axios(config)
+        .then(async function (response) {
+        console.log(JSON.stringify(response.data));
+        const mdb1 = await clienteMongo.INSERT_ONE('token_multivende', response.data)
+            console.log(mdb1)
+            res.status(200).json(response.data);
+        })
+        .catch(function (error) {
+            console.log(error);
+            res.status(404).json({
+                ok: false,
+                msg: 'Error de envio de data'
+            });
+        });
+    } catch (error) {
+        console.log(error.message)
+        res.status(404).json({
+            ok: false,
+            msg: 'Error de envio de data'
+        });
+    }
+}
+
 module.exports = {
+    refresh_token,
     token
 }
--- api/routes/general.js
+++ api/routes/general.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
 const { notify } = require('../controllers/general');
-const { token } = require('../controllers/config');
+const { refresh_token, token } = require('../controllers/config');
 
 /* realizar pedido */
 router.post('/notify?:code', notify);
+router.get('/refresh_token', refresh_token);
 router.get('/token', token);
 
 module.exports = router;
